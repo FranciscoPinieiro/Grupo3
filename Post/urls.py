0b07fb875c66c775c67c995fc9112c9b30f14370
Francisco Piñeiro Rossi <panchopr_91@hotmail.com>
2022-01-15 13:57:51 -0300

    Primer commit con modelos del after de franco

---
+++ Post/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from Post import views
+
+urlpatterns = [
+    path('',views.posts, name='Post'),
+    path('showPost/<id>',views.showPost, name='ShowPost'),
+    path('comment/<id>',views.comment, name='Comment'),
+]
